1c7c610098519d508889c1f060d0b24ac1326eef
sharmaaman4419 <sharmaaman4419@gmail.com>
2019-10-22 22:14:21 +0530

    Minor Changes

--- src/Components/Readarticle.js
+++ src/Components/Readarticle.js
@@ -5,54 +5,62 @@ class Readarticle extends React.Component {
         super(props)
         this.state = {
             Article:"",
-            commentBody:""
+            commentBody:"",
+            userComment:""
         }
     }
     componentDidMount() {
-        const slug = this.props.match.params.slug;
-        fetch(`https://conduit.productionready.io/api/articles/${slug}`,{
-            headers: {
-                'Content-Type': 'application/json'
-                // 'Content-Type': 'application/x-www-form-urlencoded',
-              },
-        }).then(res => res.json())
-        .then(data => {
-            const article = data.article;
-            this.setState({...this.state,Article:article})
-        })
-    }
+     const slug = this.props.match.params.slug;
+        Promise.all([
+            fetch(`https://conduit.productionready.io/api/articles/${slug}`,{
+                    headers: {
+                        'Content-Type': 'application/json'
+                        // 'Content-Type': 'application/x-www-form-urlencoded',
+                    },
+                }),
+                fetch(`https://conduit.productionready.io/api/articles/${slug}/comments`,{
+                    method: "GET",
+                    headers: {
+                        'Authorization':`Token ${localStorage.getItem("Token")}`,
+                        'Content-Type': 'application/json'
+                        // 'Content-Type': 'application/x-www-form-urlencoded',
+                    }
+                })
+        ])
+        .then(res => {
+                res[0].json().then(article=>this.setState({...this.state,Article:article}))
+                res[1].json().then(comments=>this.setState({...this.state,userComment:comments}))
+            }
+        )
+}
+    
     toUpdate = (event) => {
         const comment = event.target.value;
         this.setState({...this.state,commentBody:comment})
     }
-    addComment = () =>  {
-        const comments = {
-            "comment": {
-                "body":this.state.commentBody
-            }
-        }
-        const Slug = this.props.match.params.slug;
-        console.log(Slug)
-        fetch(`https://conduit.productionready.io/api/articles/${Slug}/comments`,{
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-                // 'Content-Type': 'application/x-www-form-urlencoded',
-              },
-              body:JSON.stringify(comments)
-        }).then(res => res.json())
-        .then(data => {
-            console.log(data)
-        })
-    }
+    // addComment = () =>  {
+    //     const comments = {
+    //         "comment": {
+    //             "body":this.state.commentBody
+    //         }
+    //     }
+    //     const Slug = this.props.match.params.slug;
+    //     console.log(Slug)
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         const comment = data.comment.body;
+    //         this.setState({...this.state,userComment:comment,commentBody:''})
+    //     })
+    //  }
     render() {
         // var {article} = this.props.history.location
-        console.log("reached1")
+        console.log(this.state)
         return(
             <>
             <h1>{this.state.Article.body}</h1>
-            <input type="text" placeholder="AddComment" value = {this.state.commentBody} onChange = {this.toUpdate}/>
+            <input className="input" type="text" placeholder="AddComment" value = {this.state.commentBody} onChange = {this.toUpdate}/>
             <button onClick = {this.addComment}>AddComment</button>
+            {/* <h1>{this.state.userComment}</h1> */}
             </>
         )
     }
